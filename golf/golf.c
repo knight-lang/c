c4191aeef7750fcc22ea77d07d2f978af4d847a5
SamW <mail@sampersand.me>
2022-08-16 11:59:31 -0700

    renamed golf2 to golf

--- golf/golf.c
+++ golf/golf.c
@@ -1,56 +1,55 @@
-// note that this should compile with no warnings.
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
-#include<string.h>
 #include<math.h>
+#include<string.h>
+#include<time.h>
+#ifndef X
+#define X 99999
+#endif
+#define S(s)((char*)((s)&~15))
+#define G(x)(((l*)((v)&~15))[x])
+#define die(...)(fprintf(stderr,__VA_ARGS__),fputc(10,stderr),exit(1))
 #define l long long
-#define U(s)(s&~15)
-#define B(s)(char*)U(s)
-#define D(s)(s>>4)
-#define E(n)1|(n)<<4
-#define F(n)2|(l)n
-#define G(n)n?12:4
-#define A(n)((l*)U(v))[n]
-#define L strlen
-#define P printf
 #define W while
-#define K strcat
 #define I if
+#define C ;case
+#define K(x)C x:R
+#define Z strspn
+#define M strcmp
+#define L strlen
+#define D strndup
+#define A(x)calloc(1+(x),1)
+#define P printf
+#define J strcat
 #define R return
-#define C case
-#define Q strcmp
-#define N strspn
-#define Z strndup
-#define T A(1)
-#define V A(2)
-int H,J;char*S,*O[99999],X[24],q[2];l M[99999];size_t z,a,e,d;l p(){char*t,n[2],
-c,J;l*f,v=J=0;W(N(S,"\t\n\f\r {}[]():#"))I(*S-35)++S;else W(*S++!=10);W(isdigit(
-*S))v=v*10+*S++-(J=48);I(J)R 1|v<<4;W(islower(*n=c=*S)||isdigit(c)||95==c)J?++S:
-(J=1,t=S);I(J)R 4|(l)Z(t,S-t);I(c==34||c==39){t=++S;W(*S++-c){}R 2|(l)Z(t,S-t-1)
-;}++S;I(isupper(c))W(isupper(*S)||*S==95)++S;I(N(n,"TFN"))R c-84?c-70?8:4:12;v=8
-|(l)(f=calloc(40,1));*f=c;I (c==82||c==80)R v;f[1]=p();I(N(n,"EBC`Q!LDOA"))R v;f
-[2]=p();I(!N(n,"GIS"))R v;f[3]=p();I(c-71&&c-73)f[4]=p();R v;}l r(l);char*x(l v)
-{R v&1?sprintf(X,"%lld",v>>4),X:v&2?B(v):v<13?v-4?v-8?"true":"null":"false":x(r(
-v));}_Bool b(l v){R v&1?v>>4:v&2?*B(v):v<13?v==12:b(r(v));}l n(l v){R v&1?v>>4:v
-&2?strtoll(B(v),0,10):v<13?v==12:n(r(v));}l r(l v){l t,y=1;char*s,*g,*h;FILE*f;I
-(v&3||v<13)R v;I(v&4)for(J=H;+J--;)I(!Q(B(v),O[J]))R M[J];I(J=H,v&4)W(J--)I(!Q(B
-(v),O[J]))R M[J];switch(*(l*)U(v)){C'R':R E(rand()&65535);C'P':s=0;getline(&s,&z
-,stdin);R F(Z(s,L(s)));C'E':S=x(T);R r(p());C'B':R T;C'C':R r(r(T));C'`':f=popen
-(x(T),"r");s=malloc(e=2048);W((a=fread(s+d,1,e-d,f)))I((d+=a)==e)s=realloc(s,e*=
-2);R F(s);C'Q':exit(n(T));C'!':R G(!b(T));C'L':R E(L(x(T)));C'D':I((v=r(T))&1)R
-P("Number(%lld)",v>>4),v;I(v&2)R P("String(%s)",B(v)),v;I(v-8)R P("Boolean(%s)",
-v-12?"false":"true"),v;R P("Null()"),v;C'O':I((z=L(s=x(T)))&&s[--z]=='\\')P("%."
-"*s",(int)z,s);else puts(s);R 8;C'A':I(2&(t=r(T)))R E(*B(t));*q=D(t);R F(Z(q,1))
-;C'+':I(1&(t=r(T)))R E(D(t)+n(V));s=B(t);g=x(V);R F(K(K(calloc(1+L(s)+L(g),1),s)
-,g));C'-':R E(n(T)-n(V));C'*':I((t=r(T))&1)R E(D(t)*n(V));s=B(t);g=malloc(1+L(s)
-*(t=n(V)));for(*g=0;t;--t)K(g,s);R F(g);C+'/':R E(n(T)/n(V));C'%':R E(n(T)%n(V))
-;C'^':R E((l)pow(n(T),n(V)));C'<':R G((1&(t=r(T)))?D(t)<n(V):2&t?Q(B(t),x(V))<0:
-b(V)&&t!=12);C'>':R G((1&(t=r(T)))?D(t)>n(V):2&t?Q(B(t),x(V))>0:!b(V)&&t==12);C
-'?':R G((2&(t=r(T)))?2&(y=r(V))&&!Q(B(t),B(y)):t==r(V));C'&':R b(t=r(T))?r(V):t;
-C'|':R b(t=r(T))?t:r(V);C';':R r(T),r(V);C'=':t=r(V);for(J=0;J<H;++J)I(!Q(B(T),O
-[J]))R M[J]=t;R O[H]=B(T),M[H++]=t;C'W':W(b(T))r(V);R 8;C'I':R r(A(b(T)?2:3));C
-'G':s=x(T)+n(V);R F(Z(s,n(A(3))));C'S':g=x(T);t=n(V);y=n(A(3));h=x(A(4));R F(K(K
-(strncat(calloc(L(g)+L(h)+y+1+t,1),g,t),h),g+y+t));}R 1;}int main(int c,char**v)
-{I(!c)R fprintf(stderr,"usage: %s (-e 'program' | -f file)\n",*v),1;I('f'-v[1][1
-])S=v[2];else getdelim(&S,&z,0,fopen(v[2],"r"));srand((int)v);r(p());}//golf.c
+#define O G(1)
+#define T G(2)
+char*j,*k,*B[]={"false","true","null"},*U[X],E[22];l i,V[X],r(l);size_t x,y,z;l
+p(){l*f,v=0;char*t=0,u[2]={0},c;W(Z(j,"\t\f\r\n{}[]():# "))I(*j++==35)W(*j-10)j
+++;W(isdigit(*j))t=j,v=v*10+*j++-48;I(t)R 1|v<<4;W(islower(c=*j)||isdigit(c)|95
+==c)t?++j:(t=j);I(t){for(v=j-t,i=0;(k=U[i])&&v!=(l)L(k)|strncmp(k,t,v);++i);I(!
+U[i])U[i]=D(t,v);R(l)&V[i]|4;}t=++j;I(c==39|c==34){W(*j++!=c);R(l)D(t,j-t-1)|2;
+}*u=c;W(isupper(c)&&isupper(*j)|*j==95)++j;I(Z(u,"TFN"))R+ c-84?c-70?12:0:8;v=8
+|(l)(f=A(5*sizeof(l)-1));*f++=c;I(c-82&&c-80)I(*f++=p(),!Z(u,"EBC`Q!LDOA~"))I(*
+f++=p(),Z(u,"GIS"))I(*f++=p(),c>82)*f=p();R v;}l b(l v){R 0<=v&v<13?v==8:v&2?*S
+(v)!=0:v&1||b(r(v));}char*s(l v){R v&2?S(v):v&1?sprintf(E,"%lld",v>>4),E:v<13?B
+[v/5]:s(r(v));}l n(l v){R v&1?v>>4:v&2?strtoll(S(v),0,10):v<13?v==8:n(r(v));} l
+r(l v){l t,u; I(v<13|v&3)R v;I(v&4)R*(l*)(v-4);switch(G(0)){K(80)j=0,getline(&j
+,&z,stdin),2|(l)D(j,L(j))K(82)1|0xfffff0&rand()K(69)j=s(O),r(p())K(66)O K(67)r(
+r(O))C 96:j=A(x=2047);y=0;FILE*f=popen(s(O),"r");W(z=fread(j+y,1,x-y,f))I(x==(y
++=z))j=realloc(j,x*=2);R(l)D(j,L(j))|2 K(81)exit(n(O)),0 K(33)8*!b(O)K(76)1|16*
+L(s(O))K(68)(1&(t=r(O))?P("Number(%lld)",t>>4):2&t?P("String(%s)",S(t)):t<12?P(
+"Boolean(%s)",t?"true":"false"):P(t-12?"<?>":"Null()")),P("\n"),t K(79)z=L(j=s(
+O)),P(!z||j[z-1]-92?"%2$s\n":"%.*s",(int)z-1,j),12 K(65)1&(t=r(O))?*(j=A(1))=t/
+16,2|(l)j:1|16**S(t)K('~')1|16*-n(O)K(43)2&(t=r(O))?j=S(t),k=s(T),2|(l)J(J(A(L(
+j)+L(k)),j),k):t+16*n(T)K(45)1|16*(n(O)-n(T))C 42:I(1&(t=r(O)))R 1|(t>>4)*n(T)*
+16;k=A(L(j=S(t))*(u=n(T)));W(u--)J(k,j);R 2|(l)k K(47)1|16*(n(O)/n(T))K(37)1|16
+*(n(O)%n(T))K(94)1|(int)pow(n(O),n(T))<<4 C 60:K(62)(i=1&(t=r(O))?(t>>4)-n(T):2
+&t?M(S(t),s(T)):(t/8)-b(T)),8*(G(0)-60?i>0:i<0)K(63)8*((t=r(O))==(u=r(T))||t&u&
+2&&!M(S(t),S(u)))K(38)b(t=r(O))?r(T):t K('|')b(t=r(O))?t:r(T)K(59)r(O),r(T)K(61
+)*(l*)(O-4)=r(T)C 87:W(b(O))r(T);R 12 K(73)r(G(3-b(O)))K(71)2|(l)D(s(O)+n(T),n(
+G(3)))K(83)j=s(O),t=n(T),u=n(G(3)),k=s(G(4)),2|(l)J(J(strncat(A(L(j)+L(k)+u),j,
+t),k),j+t+u);default:die("invalid char '%c'",(int)G(0));}}int main(int c,char**
+v){srand(time(0));I(c-3)die("usage: %s (-e 'program' | -f file)\n",*v);v[1][1]-
+'f'?j=v[2],0:getdelim(&j,&z,0,fopen(v[2],"r"));r(p());}
